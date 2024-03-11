d1649b9dd26948869a63c3a4209e4ad4e59aa3f7
okochaamos <oko23015@byui.edu>
2024-03-11 18:35:40 +0100

    my commit

---
+++ scripts/w02-task.js
@@ -0,0 +1,74 @@
+/* W02-Task - Profile Home Page */
+
+
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullName = "Okocha Amos Mba";
+console.log(`fullName : ${fullName}`);
+
+var currentYear = new Date();
+console.log(`Current year : ${currentYear}`);
+
+let profilePicture = "images/placeholder.jpg";
+console.log(`${profilePicture}`);
+
+const nameElement = document.getElementById("name");
+
+const foodElement = document.getElementById("food");
+
+const yearElement = document.querySelector("#year");
+
+let imageElement = document.getElementById("home");
+
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+
+yearElement.innerTextContent = `<strong>${currentYear}</strong>`;
+
+imageElement.setAttribute("src", images/placeholder.jpg);
+
+const favoriteFood = ["rice", "eba", "yam", "plantain poridge", "fish pepper soup","goatmeat pepper soup"];
+console.log(`${favoriteFood}`)
+const myFood = favoriteFood.push("edikan ikong soup");
+console.log(`${myFood}`)
+
+const latestFood = favoriteFood.pop();
+console.log(`${latestFood}`)
+
+const myLatestFood = favoriteFood.shift();
+console.log(`${myLatestFood}`)
+
+foodElement += `<br> ${favoriteFood}`;
+
+foodElement += `<br>${favoriteFood}`;
+
+
+
+
+
+
+
+
+
+
+/* Step 3 - Element Variables */
+
+
+
+
+
+/* Step 4 - Adding Content */
+
+
+
+
+
+
+/* Step 5 - Array */
+
+
+
+
+
+
