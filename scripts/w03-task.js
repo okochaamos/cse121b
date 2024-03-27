42c85836815f104424b955138ccb775fd8eb25f5
okochaamos <oko23015@byui.edu>
2024-03-27 11:22:44 +0100

    commit

---
+++ scripts/w03-task.js
@@ -0,0 +1,46 @@
+/* LESSON 3 - Programming Tasks */
+
+
+let numbers1 = Number(document.querySelector("#add1").value);
+let numbers2 = Number(document.querySelector("#add2").value);
+/* FUNCTIONS */
+/* Function Definition - Add Numbers */
+
+
+function add(numbers1, numbers2) {
+    return numbers1 + numbers2;
+}
+
+function addNumbers(numbers1, numbers2){
+    document.querySelector("#sum")= add(numbers1, numbers2);
+
+}
+    
+
+let button = document.querySelector("#addNumbers").addEventListener(click, addNumbers);
+
+
+/* Function Expression - Subtract Numbers */
+
+
+/* Arrow Function - Multiply Numbers */
+
+
+/* Open Function Use - Divide Numbers */
+
+
+/* Decision Structure */
+
+
+/* ARRAY METHODS - Functional Programming */
+/* Output Source Array */
+
+/* Output Odds Only Array */
+
+/* Output Evens Only Array */
+
+/* Output Sum of Org. Array */
+
+/* Output Multiplied by 2 Array */
+
+/* Output Sum of Multiplied by 2 Array */
